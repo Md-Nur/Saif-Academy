7225f40fcf968f62b9c8bb1d58c2f9a617952997
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2023-07-08 21:08:43 +0600

    change the navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="bg-gray-800 fixed w-full shadow-md ">
+        <nav className="bg-gray-800 shadow-md ">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center justify-between w-full">
