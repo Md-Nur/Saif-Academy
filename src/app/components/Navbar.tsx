8889f9e6bc70b343ce56e2dd9ffb840b65e47b5e
Nur <md.nurealamsiddiquee2004@gmail.com>
2023-07-10 11:04:25 +0600

    update navbar for mobile view

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -106,16 +106,16 @@ const Navbar = () => {
                             Home
                         </Link>
                         <Link
-                            href="#"
+                            href=",/batch"
                             className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                         >
-                            About
+                            All Batches
                         </Link>
                         <Link
-                            href="#"
+                            href="./signup"
                             className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                         >
-                            Services
+                            Sign Up/Log In
                         </Link>
                         <Link
                             href="#"
